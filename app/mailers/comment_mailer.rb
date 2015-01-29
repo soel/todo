1d021128a77ab6bbfe9fa6fc5d2fb3a715920f7f
kanbara <kanbara@bbtower.co.jp>
2015-01-29 15:25:48 +0900

    fix comment mail subject

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -2,7 +2,7 @@ class CommentMailer < ActionMailer::Base
   default from: "from@example.com"
   
   def comment_email(tomail, order)
-    sub =  "コメントがありました" + order.contract_number.to_s + " " + order.customer_id.to_s
+    sub =  "コメントがありました " + order.contract_number.to_s + " " + order.customer_id.to_s
     #@orderid = order.id
     @owner = order.users[0].username
     @url = 'http://localhost:3000/orders/' + order.id.to_s
